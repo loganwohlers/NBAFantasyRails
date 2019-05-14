00a07dce77d730bff1d399ad2626081fc85aa11f
Logan Wohlers <lawohlers@gmail.com>
2019-05-13 17:45:09 -0700

    pushing up rails backend + scraping work day1

---
+++ config/environment.rb
@@ -0,0 +1,10 @@
+# Load the Rails application.
+require_relative 'application'
+
+# Initialize the Rails application.
+Rails.application.initialize!
+
+require 'bundler'
+Bundler.require
+
+require 'open-uri'
